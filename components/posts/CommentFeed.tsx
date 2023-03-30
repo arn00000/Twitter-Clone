6c8daa64305fd5578f025b0e0d75b984a8391b4b
Caaron <arn_0000@hotmail.com>
2023-03-30 10:54:55 +0800

    first commit

---
+++ components/posts/CommentFeed.tsx
@@ -0,0 +1,19 @@
+import usePosts from "@/hooks/usePosts";
+import CommentItem from "./CommentItem";
+import PostItem from "./PostItem";
+
+interface CommentFeedProps {
+  comments?: Record<string, any>[];
+}
+
+const CommentFeed: React.FC<CommentFeedProps> = ({ comments = [] }) => {
+  return (
+    <>
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} data={comment} />
+      ))}
+    </>
+  );
+};
+
+export default CommentFeed;
